996c3f2d5904b92bf5b2f490879b103959b08b65
Diego Flores <20151234@gmail.com>
2026-08-02 14:03:55 -0500

    Responder mensajes automáticamente

--- app/routers/webhook.py
+++ app/routers/webhook.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter, Request, Query
 from fastapi.responses import PlainTextResponse
 
 from app.config import VERIFY_TOKEN
+from app.services.whatsapp import WhatsAppService
 
 router = APIRouter(
     prefix="/webhook",
     tags=["WhatsApp Webhook"]
 )
+
+whatsapp = WhatsAppService()
 
 
 @router.get("")
@@ -16,33 +19,36 @@ async def verify_webhook(
     hub_challenge: str = Query(None, alias="hub.challenge"),
 ):
 
-    print("========== GET WEBHOOK ==========")
-    print("hub_mode:", hub_mode)
-    print("hub_verify_token:", hub_verify_token)
-    print("VERIFY_TOKEN:", VERIFY_TOKEN)
-    print("=================================")
-
     if hub_mode == "subscribe" and hub_verify_token == VERIFY_TOKEN:
-        print("WEBHOOK VERIFICADO")
         return PlainTextResponse(content=hub_challenge)
 
-    print("TOKEN INVALIDO")
     return PlainTextResponse("Token invalido", status_code=403)
 
 
 @router.post("")
 async def receive_message(request: Request):
 
-    print("")
-    print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
-    print("POST /webhook RECIBIDO")
-    print("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
-
     body = await request.json()
 
-    print("")
-    print("PAYLOAD COMPLETO:")
     print(body)
-    print("")
+
+    try:
+        value = body["entry"][0]["changes"][0]["value"]
+
+        if "messages" in value:
+
+            message = value["messages"][0]
+            from_number = message["from"]
+            text = message["text"]["body"]
+
+            print("MENSAJE:", text)
+
+            whatsapp.send_text(
+                to=from_number,
+                message=f"👋 Hola, recibí tu mensaje: {text}"
+            )
+
+    except Exception as e:
+        print("ERROR:", e)
 
     return {"status": "ok"}
